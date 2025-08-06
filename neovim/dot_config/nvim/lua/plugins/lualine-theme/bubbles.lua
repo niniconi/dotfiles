0cc39eb9585802a8beaba89f8f63477e507db4b2
niniconi <Stump0828Provoking2393Y@gmail.com>
2025-08-06 21:36:50 +0800

    update neovim lualine's theme

--- neovim/dot_config/nvim/lua/plugins/lualine-theme/bubbles.lua
+++ neovim/dot_config/nvim/lua/plugins/lualine-theme/bubbles.lua
@@ -29,19 +29,32 @@ local bubbles_theme = {
 require('lualine').setup {
   options = {
     theme = bubbles_theme,
-    component_separators = '|',
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    },
+    component_separators = { left = ")", right = "(" },
     section_separators = { left = '', right = '' },
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      { 'mode', separator = { right = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch' },
+    lualine_b = {
+      'filename',
+      'branch' ,
+      'diff',
+      {
+        'diagnostics',
+        symbols = { error = "󰅚 ", warn = " ", info = "󰋽 ", hint = "󰘥 " },
+      },
+    },
     lualine_c = { 'fileformat' },
     lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
+    lualine_y = { 'filetype', 'encoding', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      { 'location', separator = { left = '' }, left_padding = 2 },
     },
   },
   inactive_sections = {
