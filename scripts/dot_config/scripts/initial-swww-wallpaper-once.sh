b77d9f29c36b7fd3fe9d0149004f0dc905d55506
niniconi <Stump0828Provoking2393Y@gmail.com>
2025-09-21 16:24:51 +0800

    fix: Resolved a spelling error and a cache check

--- scripts/dot_config/scripts/initial-swww-wallpaper-once.sh
+++ scripts/dot_config/scripts/initial-swww-wallpaper-once.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cache_path=""
+monitor_name=`hyprctl monitors | grep Monitor | awk '{ print $2 }'`
+
 if [ -n "${XDG_CACHE_HOME+x}" ]; then
-    cache_path="${XDG_CACHE_HOME}/swww"
+    cache_path="${XDG_CACHE_HOME}/swww/${monitor_name}"
 elif [ -n "${HOME+x}" ]; then
-    cache_path="${HOME}/.cache/swww"
+    cache_path="${HOME}/.cache/swww/${monitor_name}"
 fi
 
-if [ ! -n "${cache_path}" ];then
+if [ ! -e $cache_path ];then
     swww img --transition-type center --transition-pos 0,0 --transition-step 90 $1
 fi
