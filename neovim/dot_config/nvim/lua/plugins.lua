274db4a4de0ec584e07e3456e4d5484fe806be93
niniconi <Stump0828Provoking2393Y@gmail.com>
2025-10-31 14:01:37 +0800

    feat: Add plugins hrsh7th/cmp-path and hrsh7th/cmp-buffer.

--- neovim/dot_config/nvim/lua/plugins.lua
+++ neovim/dot_config/nvim/lua/plugins.lua
@@ -62,6 +62,8 @@ require('lazy').setup({
             'hrsh7th/cmp-nvim-lsp',
             'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer'
         },
         config = function()
             require('plugins.nvim-cmp').setup()
