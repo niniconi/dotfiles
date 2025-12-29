5599b4d0ad096bed4c1d7fcd94955198e2715311
niniconi <Stump0828Provoking2393Y@gmail.com>
2025-12-29 23:25:00 +0800

    feat: Add plugin noice.nvim.

--- neovim/dot_config/nvim/lua/plugins.lua
+++ neovim/dot_config/nvim/lua/plugins.lua
@@ -86,7 +86,7 @@ require('lazy').setup({
             'SmiteshP/nvim-navic',
             'MunifTanjim/nui.nvim',
         },
-        config = function ()
+        config = function()
             require('plugins.nvim-navbuddy').setup()
         end
     },
@@ -224,5 +224,20 @@ require('lazy').setup({
         config = function()
             require('plugins.snacks').setup()
         end
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
     }
 })
