37a7a08904e71ad2e3b0264c0ee3aedab3b35802
niniconi <Stump0828Provoking2393Y@gmail.com>
2025-12-31 18:08:13 +0800

    feat: Add plugin lazydev.nvim.

--- neovim/dot_config/nvim/lua/plugins.lua
+++ neovim/dot_config/nvim/lua/plugins.lua
@@ -239,5 +239,16 @@ require('lazy').setup({
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
         }
-    }
+    },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
 })
