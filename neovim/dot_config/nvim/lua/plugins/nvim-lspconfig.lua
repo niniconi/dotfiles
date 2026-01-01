b0faad6854f0c57548fd3770c31eef0fb3691d17
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-01-01 20:55:48 +0800

    fix: Update diagnostic signs to use new Lua API

--- neovim/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -14,15 +14,16 @@ function M.setup()
         ensure_installed = { 'rust_analyzer', 'pyright', 'clangd', 'lua_ls', 'bashls', 'jdtls', 'phpactor', 'zls', 'ts_ls', 'asm_lsp', 'sqls', 'html', 'cssls', 'jsonls', 'yamlls' },
     })
 
-    local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-    for type, icon in pairs(signs) do
-        local hl = 'DiagnosticSign' .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
-
     vim.diagnostic.config({
         virtual_text = true,
-        signs = true,
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = ' ',
+                [vim.diagnostic.severity.WARN]  = ' ',
+                [vim.diagnostic.severity.HINT]  = ' ',
+                [vim.diagnostic.severity.INFO]  = ' ',
+            }
+        },
         update_in_insert = false,
         underline = true,
         severity_sort = true,
