cef0447f745d73e1a78f165ec46d89a59ef9d14c
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-02-01 05:15:09 +0800

    fix: nvim-treesitter not found error.

--- neovim/dot_config/nvim/lua/plugins/treesitter.lua
+++ neovim/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-    require('nvim-treesitter.configs').setup({
+    require('nvim-treesitter').setup({
         ensure_installed = { 'rust' },
         highlight = { enable = true },
         indent = { enable = true },
