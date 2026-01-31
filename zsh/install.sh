16118c95e4dee85494a20e102178e142da736704
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-02-01 07:19:06 +0800

    fix: replace wget with curl in zsh/install.sh script

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,5 +3,5 @@
 #install oh my zsh
 if [ ! -d ~/.oh-my-zsh ];then
     printf " - ${installing}:oh my zsh\n"
-    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
