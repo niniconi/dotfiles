95306368256c331a0bd4790c52b3298665cb866d
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-04-09 22:17:33 +0800

    feat: Update cpp oi-cf snippet.
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
    cout.tie(nullptr);

--- neovim/dot_config/nvim/snippets/cpp.lua
+++ neovim/dot_config/nvim/snippets/cpp.lua
@@ -40,6 +40,10 @@ void solve(){{
 }}
 
 int main(int argc, char **args){{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
   int t;
   cin >> t;
   for (int i = 0;i < t;i++)
