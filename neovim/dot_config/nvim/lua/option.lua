d772d1058d52414e2c585a61b598aa170464f83b
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-07-07 05:33:39 +0000

    fix: use vim.schedule to override Neo-tree fold settings

--- neovim/dot_config/nvim/lua/option.lua
+++ neovim/dot_config/nvim/lua/option.lua
@@ -1,4 +1,16 @@
 vim.opt.backup = false
-vim.cmd([[
-set foldmethod=indent
-]])
+
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = true
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "neo-tree",
+  },
+  callback = function()
+    vim.schedule(function()
+      vim.opt_local.foldmethod = "manual"
+      vim.opt_local.foldenable = false
+    end)
+  end,
+})
