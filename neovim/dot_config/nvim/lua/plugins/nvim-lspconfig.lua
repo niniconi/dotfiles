213b448f7fc875cae9869a32130681902a94948e
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-07-07 15:57:10 +0000

    refactor: simplify lsp config with vim.lsp.config('*') wildcard pattern
    - Replace per-server on_attach repetition with global '*' wildcard
    - Remove 9 redundant vim.lsp.config() calls for unmodified servers
    - Unify capabilities definition in one place instead of 3 repeats
    - Drop default_lsp_config intermediate variable
    - Keep only rust_analyzer, lua_ls, dartls, yamlls custom overrides

--- neovim/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -8,28 +8,6 @@ local on_attach = function(client, bufnr)
 end
 
 function M.setup()
-  require("mason").setup()
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-      "rust_analyzer",
-      "pyright",
-      "clangd",
-      "lua_ls",
-      "bashls",
-      "jdtls",
-      "phpactor",
-      "zls",
-      "ts_ls",
-      "asm_lsp",
-      "sqls",
-      "html",
-      "cssls",
-      "jsonls",
-      "yamlls",
-      "marksman",
-    },
-  })
-
   vim.diagnostic.config({
     virtual_text = true,
     signs = {
@@ -52,12 +30,37 @@ function M.setup()
 
   vim.lsp.inlay_hint.enable(true)
 
-  local default_lsp_config = {
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  vim.lsp.config("*", {
     on_attach = on_attach,
-  }
+    capabilities = capabilities,
+  })
+
+  require("mason").setup()
+  require("mason-lspconfig").setup({
+    ensure_installed = {
+      "rust_analyzer",
+      "pyright",
+      "clangd",
+      "lua_ls",
+      "bashls",
+      "jdtls",
+      "phpactor",
+      "zls",
+      "ts_ls",
+      "asm_lsp",
+      "sqls",
+      "html",
+      "cssls",
+      "jsonls",
+      "yamlls",
+      "marksman",
+    },
+  })
 
   vim.lsp.config("rust_analyzer", {
-    on_attach = on_attach,
     settings = {
       ["rust-analyzer"] = {
         cargo = {
@@ -76,22 +79,13 @@ function M.setup()
       },
     },
   })
-  vim.lsp.config("pyright", default_lsp_config)
-  vim.lsp.config("clangd", default_lsp_config)
-  vim.lsp.config("bashls", default_lsp_config)
-  vim.lsp.config("jdtls", default_lsp_config)
-  vim.lsp.config("phpactor", default_lsp_config)
-  vim.lsp.config("zls", default_lsp_config)
-  vim.lsp.config("ts_ls", default_lsp_config)
-  vim.lsp.config("asm_lsp", default_lsp_config)
+
   vim.lsp.config("dartls", {
-    on_attach = on_attach,
     cmd = { "fvm", "dart", "language-server", "--protocol=lsp" },
   })
   vim.lsp.enable("dartls")
 
   vim.lsp.config("lua_ls", {
-    on_attach = on_attach,
     on_init = function(client)
       if client.workspace_folders then
         local path = client.workspace_folders[1].name
@@ -105,33 +99,17 @@ function M.setup()
 
       client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
         runtime = {
-          -- Tell the language server which version of Lua you're using (most
-          -- likely LuaJIT in the case of Neovim)
           version = "LuaJIT",
-          -- Tell the language server how to find Lua modules same way as Neovim
-          -- (see `:h lua-module-load`)
           path = {
             "lua/?.lua",
             "lua/?/init.lua",
           },
         },
-        -- Make the server aware of Neovim runtime files
         workspace = {
           checkThirdParty = false,
           library = {
             vim.env.VIMRUNTIME,
-            -- Depending on the usage, you might want to add additional paths
-            -- here.
-            -- '${3rd}/luv/library'
-            -- '${3rd}/busted/library'
           },
-          -- Or pull in all of 'runtimepath'.
-          -- NOTE: this is a lot slower and will cause issues when working on
-          -- your own configuration.
-          -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-          -- library = {
-          --   vim.api.nvim_get_runtime_file('', true),
-          -- }
         },
       })
     end,
@@ -140,25 +118,7 @@ function M.setup()
     },
   })
 
-  vim.lsp.config("sqls", default_lsp_config)
-
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-  vim.lsp.config("html", {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-  vim.lsp.config("cssls", {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
-  vim.lsp.config("jsonls", {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
   vim.lsp.config("yamlls", {
-    on_attach = on_attach,
     settings = {
       yaml = {
         schemas = {
