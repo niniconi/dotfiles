ec68e5a21fdae674c207b89d1f1633806fdf3e04
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-08-21 22:43:42 +0800

    chore: remove obsolete .meta files and old install scripts (pre-NixOS)

--- zsh/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#install oh my zsh
-if [ ! -d ~/.oh-my-zsh ];then
-    printf " - ${installing}:oh my zsh\n"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
