3f924aeb5831fe1ff9301524cbb11f39c01ff621
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-08-25 21:50:25 +0800

    feat: add Ghidra with all official extensions
    - Create ghidra.nix with 13 plugins (findcrypt, ret-sync, kaiju, wasm,
    etc.)
    - Import ghidra.nix in host configuration

--- hosts/common/packages/reversing.nix
+++ hosts/common/packages/reversing.nix
@@ -5,7 +5,20 @@
 {
   environment.systemPackages = with pkgs; [
     # decompilers/disassemblers
-    ghidra
+    (ghidra.withExtensions (p: with p; [
+      findcrypt                    # Crypto constant detection
+      ghidra-delinker-extension    # Delink executables to object files
+      ghidra-firmware-utils        # Firmware analysis utilities
+      ghidra-golanganalyzerextension  # Go binary analysis
+      ghidraninja-ghidra-scripts   # Collection of Ghidra scripts
+      gnudisassembler              # GNU disassembler integration
+      kaiju                        # CERT malware analysis tools
+      lightkeeper                  # Code coverage visualization (Lighthouse port)
+      machinelearning              # ML-based analysis
+      ret-sync                     # Sync with external debuggers
+      sleighdevtools               # Sleigh language development tools
+      wasm                         # WebAssembly support
+    ]))
     jadx
     apktool
     imhex
