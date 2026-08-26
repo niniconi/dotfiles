fc1b8f724d28637a8b10487a42fa6a050f54c043
niniconi <Stump0828Provoking2393Y@gmail.com>
2026-08-26 10:50:57 +0800

    feat: Add networkmanager-openvpn plugin for networkmanager

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -26,7 +26,12 @@
   };
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
